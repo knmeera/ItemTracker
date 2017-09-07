e6cb78ce71b4a1a7ecdae668b067a96d89862037
venu <venukumarvit@gmail.com>
2017-09-07 15:11:50 +0530

    Validation for Text Boxs

--- ClassDemo/Models/TrackerItem.cs
+++ ClassDemo/Models/TrackerItem.cs
@@ -11,12 +11,12 @@ namespace ClassDemo.Models
         List<TrackerItem> lstItems = new List<TrackerItem>();
 
         public int ItemId { set; get; }
-        public string ItemSummary { set; get; }
+        public string ItemSummary  { set; get; }
         public int ItemCategory { set; get; }
         public int ItemType { set; get; }
         public string Priority { set; get; }
         public DateTime ItemCreatedDate { set; get; }
-        public string CreatedByID { set; get; }
+        public string CreatedBy { set; get; }
         public string Owner { set; get; }
         public string Impact { set; get; }
         public string Resolution { set; get; }
