dceeaf92496df22b3f339128daa7d31c77e11853
venu <venukumarvit@gmail.com>
2017-09-12 16:49:40 +0530

    Model update

---
+++ ClassDemo/Models/ItemStatus.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ClassDemo.Models
+{
+    public class ItemStatus
+    {
+       
+        public int ItemStatusId { set; get; }
+        public string ItemStatusName { set; get; }
+
+        public List<ItemStatus> GetItemStatus()
+        {
+            List<ItemStatus> lstSts = new List<ItemStatus>();
+            lstSts.Add(new ItemStatus { ItemStatusId = 1, ItemStatusName = "Open" });
+            lstSts.Add(new ItemStatus { ItemStatusId = 2, ItemStatusName = "Assigned" });
+            lstSts.Add(new ItemStatus { ItemStatusId = 3, ItemStatusName = "Resolved" });
+            lstSts.Add(new ItemStatus { ItemStatusId = 4, ItemStatusName = "Cancel" });
+            return lstSts;
+        }
+        
+    }
+}
